12943ac7b428a9300da390f4475cbf3db7fec35a
hardikmishra0910 <142745304+hardikmishra0910@users.noreply.github.com>
2023-08-30 00:33:43 +0530

    Add files via upload

---
+++ lecture 2/Nameste.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a= 5;
+  int b=3;
+  cout<<(a>=5 && b<=3);
+  // double d=5.7;
+  // int x = (int)d +2;
+  // cout<<x<<endl;
+
+  // char ch=97;
+  // cout<< ch << endl;
+  // int num='b';
+  // cout << num << endl;
+  // long long a=5;
+  // cout<<sizeof(a) << endl;
+  // cout<<"the value of a : " << a << endl;
+
+  // char ch='a';
+  // cout<<"the value of ch: "<< ch<<endl;
+  // char ch =  256;
+
+  bool flag= true;
+  // cout<<"Namaste bharat\n"<<endl;
+  // cout<<'\n';
+  // cout<<"enter a number"<<endl;
+  // int a;
+  //taking input
+  // cin>>a;
+
+  // cout<<"You entered" << a<< endl;
+}
